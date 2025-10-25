5afa20385c06af161167a0001435409f857c4280
Gabriel Belanger <gabriel@goosebyte.games>
2025-10-25 00:36:44 -0400

    Added info about the project and added the appropriate link to the project

--- script.js
+++ script.js
@@ -36,13 +36,23 @@ const allTechnologies = {
   ]
 };
 
+// Skill appris 
+// Mechanics implémenté 
+// Tâches effectués sur le projet 
+
 const allGames = {
   list: [
     {
       name: "THE SIGNAL: Stranded on Sirenis",
-      date: "October 2025",
+      date: "Janurary 2025 ~ October 2025",
       technologies: [5, 1, 7, 8],
       description: "A survival crafting extraction shooter game where players explore diverse environments, gather resources, build bases and strategically combat on the alien planet.",
+      team: "21",
+      role: "Gameplay Programmer",
+      genre: ["Survival", "Crafting", "Extraction shooter", "Base building"],
+      style: ["Science fiction", "Realistic", "Stylize"],
+      link: "https://store.steampowered.com/app/1830110/THE_SIGNAL_Stranded_on_Sirenis/",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -59,13 +69,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "The Melty Way",
-      date: "March 2026",
+      date: "December 2022 ~ March 2026",
       technologies: [6, 0, 7, 8, 10],
       description: "A puzzle-platformer where players control a shape-shifting slime, navigating vibrant levels, solving puzzles, avoiding traps and using creative movement to explore hidden paths and overcome challenges.",
+      team: "2",
+      role: "Game Programmer, Artist, Game Design, Marketer",
+      genre: ["Plateformer", "Puzzle", "Adventure"],
+      style: ["Pixel Art", "Fantasy"],
+      link: "https://store.steampowered.com/app/3990570/The_Melty_Way/",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -82,13 +99,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Citywars Savage",
-      date: "March 2021",
+      date: "Janurary 2021 ~ March 2021",
       technologies: [6],
       description: "An indie MMORPG combining fast-paced combat, sandbox building, questing and cooperative PvE. With classless progression, gear upgrades and community-driven content creation.",
+      team: "8",
+      role: "Gameplay Programmer, Game Design",
+      genre: ["MMORPG", "Sandbox", "Crafting", "Building"],
+      style: ["Cartoon", "Cubic"],
+      link: "https://store.steampowered.com/app/910800/Citywars_Savage/",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -105,13 +129,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Zero Six Booting",
-      date: "April 2021",
+      date: "March 2021 ~ April 2021",
       technologies: [6, 0, 7, 10],
       description: "A swift ninja armed with a deadly spear use a grappling hook, slow down time and rewind time in this intense, action-adventure plateformer.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Hack 'n' Slash", "Time manipulation", "Plateformer"],
+      style: ["Pixel Art", "Science fiction"],
+      link: "https://betteromra.itch.io/zero-six-booting",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -128,13 +159,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Dream Catcher",
-      date: "April 2024",
+      date: "Feburary 2024 ~ April 2024",
       technologies: [6, 0, 7],
       description: "In this whimsical multiplayer platformer, you play as a tiny snail journeying through surreal dreamscapes. Work together with your friend to collect scattered dreams and keep the dreamer peacefully asleep.",
+      team: "8",
+      role: "Gameplay Programmer",
+      genre: ["Plateformer", "Coop"],
+      style: ["Vector art", "Fantasy"],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -151,13 +189,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "The Orc Cook",
-      date: "January 2021",
+      date: "December 2020 ~ January 2021",
       technologies: [6, 0, 10],
       description: "An orc chef combines ingredients in precise sequences to create hearty meals. Each successful combination, boosts reputation and satisfies the tribe's hunger in a puzzle of timing and culinary mastery.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Match-3", "Puzzle"],
+      style: ["Vector art", "Fantasy"],
+      link: "https://betteromra.itch.io/the-orc-cook",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -174,13 +219,50 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
+    },
+    {
+      name: "Sightline",
+      date: "17 Feburary 2023 ~ 20 Feburary 2023",
+      technologies: [6, 0, 10],
+      description: "Play as a cunning outlaw in the Wild West, planning heists, dodging sheriffs and looting banks for fortune and freedom.",
+      team: "3",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Stealth", "Extraction", "Time manipulation"],
+      style: ["Far west", "Vector art"],
+      link: "",
+
+      content: ["<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>",
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Plateformer Squelletron",
-      date: "December 2020",
+      date: "October 2020 ~ December 2020",
       technologies: [6, 0, 7, 10],
       description: "A skeleton traverses an endlessly generated world, jumping across platforms, avoiding dangers and exploring haunting, ever-changing landscapes full of surprises.",
+      team: "2",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Plateformer", "Procedural generation"],
+      style: ["Dark Fantasy", "Vector art"],
+      link: "https://toominio.itch.io/sightline",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -197,13 +279,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Sorcière avec éclair de feu versus Chevaliers avec nimbus 2001 sauf que les balais sont interdit",
-      date: "October 2020",
+      date: "September 2020 ~ October 2020",
       technologies: [6, 0],
       description: "A strategic, turn-based adventure where you, a witch, navigate enemies, plan moves carefully and reach the finish line triumphantly.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Turn-Based", "Stealth"],
+      style: ["Fantasy"],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -220,13 +309,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Attaque des patates",
-      date: "December 2020",
+      date: "October 2020 ~ December 2020",
       technologies: [6, 0],
       description: "A humble village boy sets out on a thrilling RPG adventure, exploring vast lands, making loyal friends, engaging in strategic turn-based battles and confronting a powerful necromancer to save the world.",
+      team: "4",
+      role: "Game Programmer",
+      genre: ["RPG", "Turn-Based"],
+      style: ["Dark Fantasy", "Vector art"],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -243,13 +339,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Mille Etage",
-      date: "November 2020",
+      date: "November 2020 ~ December 2020",
       technologies: [6, 0],
       description: "Build and manage your hotel empire, upgrade floors, attract guests, optimize profits, automate operations and watch income grow effortlessly.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Idle"],
+      style: [],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -266,13 +369,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Hinvie Zible",
-      date: "October 2019",
+      date: "November 2019 ~ December 2019",
       technologies: [6, 0, 10],
       description: "Small prototype that lets you be a ghost in a graveyard and use different ability.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: [],
+      style: [],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -289,13 +399,20 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     },
     {
       name: "Tower Defense",
-      date: "March 2017",
+      date: "March 2017 ~ April 2027",
       technologies: [6, 0],
       description: "My first ever game, a tower defense where you strategically place towers, stop enemies, survive waves, defend your base.",
+      team: "1",
+      role: "Game Programmer, Artist, Game Design",
+      genre: ["Tower defense"],
+      style: [],
+      link: "",
+
       content: ["<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
@@ -312,7 +429,8 @@ const allGames = {
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
         "<h3>Introduction</h3>",
-        "<h3>Introduction</h3>"]
+        "<h3>Introduction</h3>"],
+      learned: ``
     }
   ]
 };
@@ -324,15 +442,15 @@ document.querySelector('.game-content-space').style.position = "absolute";
 parent = 'games';
 
 for (let i = 0; i < allGames.list.length; i++) {
-  const game = allGames.list[i];
-  addGame(parent, game.name, game.date, game.technologies, game.description, game.content);
+  let game = allGames.list[i];
+  addGame(parent, game);
 }
 
 parent = 'technologies';
 
 for (let i = 0; i < allTechnologies.list.length; i++) {
   const technology = allTechnologies.list[i];
-  addTechnology(parent, technology.name);
+  addTechnology(parent, technology);
 }
 
 const gamesThumbnail = document.querySelectorAll('.game-thumbnail');
@@ -342,7 +460,7 @@ animationId = 0;
 gamesThumbnail.forEach(gameThumbnail => {
   gameThumbnail.addEventListener('click', () => {
 
-    const game = gameThumbnail.parentElement;
+    let game = gameThumbnail.parentElement;
     const gamesToRemoveOpen = document.querySelectorAll('.open');
 
     gamesToRemoveOpen.forEach(gameToRemoveOpen => {
@@ -394,31 +512,32 @@ function syncHeight(gameContentSpace, gameContent) {
 }
 
 
-function addGame(parent, name, date, technologies, description, content) {
+function addGame(parent, game) {
 
   const contentDiv = document.querySelector("." + parent);
 
-  const trimmerName = name.replace(/[^a-zA-Z0-9]/g, "");
+  // thumbnail
+  const trimmerName = game.name.replace(/[^a-zA-Z0-9]/g, "");
 
   imageThumbnail = "background-image: url('Images/Thumbnails/" + trimmerName + " main.jpg" + "');"
   imageThumbnailOver = "background-image: url('Images/Thumbnails/" + trimmerName + " gameplay.jpg" + "');"
 
-  date = '<i>' + date + '</i>';
+  date = '<i>' + game.date + '</i>';
 
-  name = '<h2>' + name + '</h2>';
+  gameName = '<h2>' + game.name + '</h2>';
 
-  description = '<p>' + description + '</p>';
+  description = '<p>' + game.description + '</p>';
 
-  const contentThumbnail = '<div class="thumbnail-content">' + date + name + description + '</div>';
+  const contentThumbnail = '<div class="thumbnail-content">' + date + gameName + description + '</div>';
 
   imageThumbnail = '<div class="image-thumbnail" style="' + imageThumbnail + '"></div>';
   imageThumbnailOver = '<div class="image-thumbnail-over" style="' + imageThumbnailOver + '"></div>';
 
   technologiesHtml = '<div class="game-technologies">';
 
-  for (let i = 0; i < technologies.length; i++) {
+  for (let i = 0; i < game.technologies.length; i++) {
 
-    const technology = allTechnologies.list[technologies[i]];
+    const technology = allTechnologies.list[game.technologies[i]];
 
     image = '<div class="game-technology">';
     image += getImageTechnology(getNameTechnology(technology.name), "game-technology-image");
@@ -431,36 +550,46 @@ function addGame(parent, name, date, technologies, description, content) {
 
   const thumbnail = '<div class="game-thumbnail">' + technologiesHtml + contentThumbnail + imageThumbnail + imageThumbnailOver + '</div>';
 
-  contentHtml = '<div class="game-content">';
+  // Content
 
-  for (let i = 0; i < content.length; i++) {
 
-    const gameContent = content[i];
+  // genre: ["Tower defense"],
+  // style: [],
+  // link: "",
+  // team: "1",
 
-    contentHtml += gameContent;
-  }
+  contentHeaderHtml = '<div class="game-content-header">';
+  
+  contentHeaderHtml += createArrayElement(game.genre, "game-genre");
+  contentHeaderHtml += createArrayElement(game.style, "game-style");
+  
+  contentHeaderHtml += '</div>';
 
-  contentHtml += '</div>';
 
-  const game = '<div class="game">' + thumbnail + contentHtml + '</div>'
+  contentHtml = '<div class="game-content">' + contentHeaderHtml + createArrayElement(game.content, "") + '</div>';
 
-  contentDiv.innerHTML += game;
+  // Add it to the page
+
+  const gameHtml = '<div class="game">' + thumbnail + contentHtml + '</div>'
+
+  contentDiv.innerHTML += gameHtml;
 }
 
-function addTechnology(parent, name) {
+function addTechnology(parent, technology) {
 
   const contentDiv = document.querySelector("." + parent);
 
-  const image = getImageTechnology(getNameTechnology(name), "image-technology");
+  const image = getImageTechnology(getNameTechnology(technology.name), "image-technology");
 
-  name = '<p>' + name + '</p>';
+  technologyName = '<p>' + technology.name + '</p>';
 
-  const technology = '<div class="technology">' + image + name + '</div>';
+  const technologyHtml = '<div class="technology">' + image + technologyName + '</div>';
 
-  contentDiv.innerHTML += technology;
+  contentDiv.innerHTML += technologyHtml;
 }
 
 function getNameTechnology(name) {
+
   const namePlus = name.replace(/\+/g, "Plus");
   const nameSharp = namePlus.replace(/\#/g, "Sharp");
   const trimmerName = nameSharp.replace(/[^a-zA-Z0-9]/g, "");
@@ -468,7 +597,26 @@ function getNameTechnology(name) {
 
 }
 
+function createArrayElement(list, classHtml) {
+  html = "";
+  if (classHtml == "") {
+
+    for (let i = 0; i < list.length; i++) {
+      html += list[i];
+    }
+
+  } else {
+
+    for (let i = 0; i < list.length; i++) {
+      html += '<div class="' + classHtml + '">' + list[i] + '</div>';
+    }
+
+  }
+  return html
+}
+
 function getImageTechnology(name, classImage) {
+
   const imageTechnology = "background-image: url('Images/Technologies/" + name + ".png" + "');"
 
   return '<div class="' + classImage + '" style="' + imageTechnology + '"></div>';
